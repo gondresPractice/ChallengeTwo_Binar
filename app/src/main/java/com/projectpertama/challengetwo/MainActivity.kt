f23eaadd9cd435edeb3b4165366a921fd2ecac56
Andre <andreashappy29@gmail.com>
2022-03-11 11:39:02 +0700

    Challenge 2 binar

---
+++ app/src/main/java/com/projectpertama/challengetwo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.projectpertama.challengetwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+//        SearchView searchView
+//        searchView findViewById<SearchView>(R.id.searchView)
+    }
+}
